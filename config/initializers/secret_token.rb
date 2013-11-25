2e244e8dcff8b1afd2f377dedc7483330d3424e5
Julie lappano <julie.lappano@gmail.com>
2013-11-25 14:42:37 -0500

    initialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HungerGames::Application.config.secret_key_base = '3defa5678a76a994a856952b41af7624beee7b89d12774b257a05941b540a3d69f65ee1fa6c0dfe37c23bd8355cca0bda53ff9f312ee68328cdb87a279e5ba03'
