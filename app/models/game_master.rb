a63d4e1f3c8955612ef4feec2f12e412ef355b66
Julie lappano <julie.lappano@gmail.com>
2013-11-25 19:54:10 -0500

    chooses tributes

--- app/models/game_master.rb
+++ app/models/game_master.rb
@@ -1,11 +1,25 @@
 class GameMaster
 
-  def create_game(param)
+  def initialize(param)
     @game = Game.create(name: param)
   end
 
-  def choose_tributes
-    @tributes
+  def game
+    @game
   end
 
+  def game_id
+    @game.id
+  end
+
+  def choose_tributes
+    f_tribute = Citizen.where(district_id: (1..12).to_a, gender: "f", age: (12..18).to_a).sample(1)[0]
+      f_tribute.type = "Tribute"
+      f_tribute.game_id = self.game_id
+      f_tribute.save
+    m_tribute = Citizen.where(district_id: [1,2,3,4,5,6,7,8,9,10,11,12], gender: "m", age: (12..18).to_a).sample(1)[0]
+      m_tribute.type = "Tribute"
+      m_tribute.game_id = self.game_id
+      m_tribute.save
+  end
 end
--- spec/models/game_master_spec.rb
+++ spec/models/game_master_spec.rb
@@ -13,25 +13,54 @@ describe GameMaster do
 
   #it creates a game
   
-  let(:gameMaker) {GameMaster.new}
+  let(:gameMaker) {GameMaster.new("The Rumble")}
   
   describe "#create_game" do
     it "creates the game" do 
-      expect(gameMaker.create_game("The Rumble")).to_not eq(nil)
+      expect(gameMaker.game).to_not eq(nil)
     end
   end
 
     #it chooses randomly two citizens 
 
   describe '#choose_tributes' do
-    let(:game) {gameMaker.create_game("The Rumble")}
-    it "chooses 4 tributes" do
-      mihran = Citizen.new(name:"Mihran Abrahamian", age: 15, gender:"m", district_id:1)
-      julie = Citizen.new(name:"Julie Lappano", age: 12, gender:"f", district_id:2)
-      raleigh = Citizen.new(name:"Raleigh Foeber", age: 16, gender:"f", district_id:3)
-      tom = Citizen.new(name:"Tom Brennan", age: 16, gender:"m", district_id:4)
+    before do 
+      mihran = Citizen.create(name:"Mihran Abrahamian", age: 15, gender: "m", district_id:1)
+      julie = Citizen.create(name:"Julie Lappano", age: 12, gender:"f", district_id:2)
+      raleigh = Citizen.create(name:"Raleigh Foeber", age: 16, gender:"f", district_id:14)
+      tom = Citizen.create(name:"Tom Brennan", age: 16, gender:"m", district_id:15)
+      harry = Citizen.create(name:"Harry Potter", age: 12, gender:"m", district_id:16)
+      pj = Citizen.create(name:"PJ hughes", age: 16, gender:"m", district_id:2)
+      frank = Citizen.create(name:"Frank Baum", age: 17, gender:"m", district_id:2)
+      jeff = Citizen.create(name:"Jeff Baum", age: 17, gender:"m", district_id:4)
+    end 
+
+    let(:game) {gameMaker.game}
+
+    it "chooses 2 random tributes" do
       gameMaker.choose_tributes
-      expect(game.tributes.count).to eq(4)
+      expect(game.tributes.count).to eq(2)
+    end
+
+    it "only chooses citizens from districts 1-12" do
+      gameMaker.choose_tributes
+      #game.tributes == [#<Tribute id: 192, name: "Mihran Abrahamian", age: 15, gender: "m", type: "Tribute", alive?: true, district_id: 1, rating: nil, game_id: 79>,
+      #<Tribute id: 193, name: "Julie Lappano", age: 12, gender: "f", type: "Tribute", alive?: true, district_id: 2, rating: nil, game_id: 79>]
+      expect(game.tributes[0].district_id).to be <= 12
+      expect(game.tributes[1].district_id).to be <= 12
+    end
+
+    it "chooses one male and one female" do
+      gameMaker.choose_tributes
+      expect([game.tributes[0].gender, game.tributes[1].gender]).to match_array(["m", "f"])
+    end
+
+    it "chooses only from ages 12 - 18" do
+      gameMaker.choose_tributes
+      expect((12..18).to_a).to include((game.tributes[0].age), (game.tributes[1].age))
+    end
+
+    it "assigns a rating randomly to each tribute" do
     end
   end
 
