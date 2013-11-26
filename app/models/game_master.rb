1531ec14db90f94a187a1939826be37345f07c62
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-26 14:17:27 -0500

    refactored game master spec and rb

--- app/models/game_master.rb
+++ app/models/game_master.rb
@@ -1,5 +1,4 @@
 class GameMaster
-
   def initialize(param)
     @game = Game.create(name: param)
   end
@@ -8,30 +7,20 @@ class GameMaster
     @game
   end
 
-  def game_id
-    @game.id
-  end
-
   def choose_tributes
-    f_tribute = Citizen.where(district_id: (1..12).to_a, gender: "f", age: (12..18).to_a).sample(1)[0]
-    f_tribute.type = "Tribute"
-    f_tribute.game_id = self.game_id
-    f_tribute.rating = (1..12).to_a.sample
-    f_tribute.save
-    f_tribute = f_tribute.becomes Tribute
-      
-    m_tribute = Citizen.where(district_id: (1..12).to_a, gender: "m", age: (12..18).to_a).sample(1)[0]
-    m_tribute.type = "Tribute"
-    m_tribute.game_id = self.game_id
-    m_tribute.rating = (1..12).to_a.sample
-    m_tribute.save
-    m_tribute = m_tribute.becomes! Tribute
-    @tributes = [f_tribute, m_tribute]
+    ["m", "f"].each do |x|
+      tribute = Citizen.where(district_id: (1..12).to_a, gender: x, age: (12..18).to_a).sample(1)[0]
+      tribute.type = "Tribute"
+      tribute.game_id = self.game.id
+      tribute.rating = (1..12).to_a.sample
+      tribute.save
+      tribute = tribute.becomes Tribute
+    end  
   end
 
   def get_sponsors 
     @sponsors = Citizen.where(type: "Sponsor")
-    @tributes.each do |tribute|
+    game.tributes.each do |tribute|
       tribute.sponsorships << Sponsorship.create(sponsor_id: @sponsors.sample(1)[0].id, tribute_id: tribute.id)
     end
   end
